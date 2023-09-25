fe2b4f74d613701c44eda6fd37a78e467dd719cc
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2023-09-25 03:15:07 +0300

    add styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Container } from './App.styled';
+
+class App extends Component {
+  state = {};
+  render() {
+    return <Container></Container>;
+  }
+}
+
+export default App;
